32662a0f7478e17e6f27d4d1f43b373af77f1045
Daniel Mackle <danmackle05@gmail.com>
2024-12-08 19:35:08 +0000

    Added all other questions

--- TreeNode.h
+++ TreeNode.h
@@ -5,29 +5,42 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
+template<typename T>
 class TreeNode {
-    public:
-    explicit TreeNode(int value = 0, TreeNode<T>* left = nullptr, TreeNode<T>* right = nullptr) : value(value), left(left), right(right) {}
-    ~TreeNode() = default;
+public:
+    explicit TreeNode(int value = 0, TreeNode<T> *left = nullptr, TreeNode<T> *right = nullptr) : value(value),
+        left(left), right(right) {
+    }
+
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
+
     T getValue() {
         return value;
     }
+
     void setValue(T value) {
         this->value = value;
     }
-    TreeNode<T>*& getLeft() {
+
+    TreeNode<T> *&getLeft() {
         return left;
     }
-    TreeNode<T>*& getRight() {
+
+    TreeNode<T> *&getRight() {
         return right;
     }
-    void setLeft(TreeNode<T>* left) {
+
+    void setLeft(TreeNode<T> *left) {
         this->left = left;
     }
-    void setRight(TreeNode<T>* right) {
+
+    void setRight(TreeNode<T> *right) {
         this->right = right;
     }
+
     void toString() {
         if (this->left) {
             left->toString();
@@ -37,8 +50,39 @@ class TreeNode {
             right->toString();
         }
     }
-    private:
+
+    int max() {
+        return max(0);
+    }
+
+    int max(int max) {
+        if (value > max) {
+            max = value;
+        }
+        if (left) {
+            left->max(max);
+        }
+        if (right) {
+            right->max(max);
+        }
+        return max;
+    }
+
+    int sum() {
+        if (left && right) {
+            return value + left->sum() + right->sum();
+        }
+        if (left) {
+            return value + left->sum();
+        }
+        if (right) {
+            return value + right->sum();
+        }
+        return value;
+    }
+
+private:
     T value;
-    TreeNode<T>* left;
-    TreeNode<T>* right;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
 };
--- main.cpp
+++ main.cpp
@@ -2,24 +2,33 @@
 #include "TreeNode.h"
 
 int main();
-void construct(const std::vector<int>& array);
-void construct(const std::vector<int>& array, int first, int last);
 
-template <typename T>
-void construct(TreeNode<T>*& root, T value);
+// NOLINTNEXTLINE(bugprone-infinite-recursion)
+void construct(const std::vector<int> &array);
 
-TreeNode<int>* root = nullptr;
+// NOLINTNEXTLINE(bugprone-infinite-recursion)
+void construct(const std::vector<int> &array, int first, int last);
+
+// NOLINTNEXTLINE(bugprone-infinite-recursion)
+template<typename T>
+void construct(TreeNode<T> *&root, T value);
+
+TreeNode<int> *root = nullptr;
 
 int main() {
-    const std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const std::vector<int> array = {51, 5, 35, 23, 28, 60};
     construct(array);
     root->toString();
+    std::cout<<std::endl<<"Max: "<<root->max()<<std::endl<<"Sum: " <<root->sum() << std::endl<<"Algorithms which traverse a built balanced BST have a time complexity of O(log(N)";
+    delete root;
     return 0;
 }
-void construct(const std::vector<int>& array) {
-    construct(array, 0, array.size()-1);
+
+void construct(const std::vector<int> &array) {
+    construct(array, 0, static_cast<int>(array.size() - 1));
 }
-void construct(const std::vector<int>& array, const int first, const int last) {
+
+void construct(const std::vector<int> &array, const int first, const int last) {// NOLINT(*-no-recursion)
     if (first > last) {
         return;
     }
@@ -28,12 +37,12 @@ void construct(const std::vector<int>& array, const int first, const int last) {
 
     construct(root, selected);
 
-    construct(array, first, mid-1);
-    construct(array, mid+1, last);
+    construct(array, first, mid - 1);
+    construct(array, mid + 1, last);
 }
 
-template <typename T>
-void construct(TreeNode<T>*& root, T value) {
+template<typename T>
+void construct(TreeNode<T> *&root, T value) { // NOLINT(*-no-recursion)
     if (!root) {
         root = new TreeNode<T>(value);
         return;
@@ -42,10 +51,10 @@ void construct(TreeNode<T>*& root, T value) {
         return;
     }
     if (value < root->getValue()) {
-        TreeNode<T>*& left = root->getLeft();
+        TreeNode<T> *&left = root->getLeft();
         construct(left, value);
         return;
     }
-    TreeNode<T>*& right = root->getRight();
+    TreeNode<T> *&right = root->getRight();
     construct(right, value);
 }
